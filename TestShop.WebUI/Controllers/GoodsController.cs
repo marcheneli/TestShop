482b5389662595be5fb43db6553f2d79620fa2e7
marcheneli <marcheneli@gmail.com>
2016-01-10 13:58:45 +0300

    Added goods controller and index view for it

---
+++ TestShop.WebUI/Controllers/GoodsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestShop.WebUI.Controllers
+{
+    public class GoodsController : Controller
+    {
+        // GET: Goods
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
