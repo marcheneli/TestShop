f1795f01cbce1f614a9c1c16ead31d9f316e28dd
marcheneli <marcheneli@gmail.com>
2016-01-10 20:02:48 +0300

    Added code to index action of goods controller for reading database

--- TestShop.WebUI/Controllers/GoodsController.cs
+++ TestShop.WebUI/Controllers/GoodsController.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestShop.WebUI.Models;
 
 namespace TestShop.WebUI.Controllers
 {
@@ -13,6 +14,8 @@ namespace TestShop.WebUI.Controllers
         // GET: Goods
         public ActionResult Index()
         {
+            List<Product> products = new List<Product>();
+
             using (IDbConnection connection = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=TestShop;Integrated Security=True;"))
             {
                 IDbCommand command = new SqlCommand("SELECT * FROM dbo.Products");
@@ -24,11 +27,18 @@ namespace TestShop.WebUI.Controllers
 
                 while(reader.Read())
                 {
+                    Product product = new Product()
+                    {
+                        ProductID = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Price = reader.GetDecimal(2)
+                    };
 
+                    products.Add(product);
                 }
             }
 
-            return View();
+            return View(products);
         }
     }
 }
