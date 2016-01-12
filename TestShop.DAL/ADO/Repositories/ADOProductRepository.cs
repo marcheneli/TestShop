664870ac2958eabf03ea3d14d74f64c08bfe7975
marcheneli <marcheneli@gmail.com>
2016-01-12 19:33:17 +0300

    Added ADOProductRepository class

---
+++ TestShop.DAL/ADO/Repositories/ADOProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestShop.Domain.Interfaces.Repositories;
+
+namespace TestShop.DAL.ADO.Repositories
+{
+    public class ADOProductRepository : IProductRepository
+    {
+        IDbConnection connection;
+
+        public ADOProductRepository(IDbConnection connection)
+        {
+            this.connection = connection;
+        }
+    }
+}
