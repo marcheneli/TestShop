9ac20bd33a04e84566263f602e172f84f3259c22
marcheneli <marcheneli@gmail.com>
2016-01-12 23:24:32 +0300

    Added ADOUnitOfWork class

---
+++ TestShop.DAL/ADO/ADOUnitOfWorkcs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestShop.Domain.Interfaces;
+using TestShop.Domain.Interfaces.Repositories;
+
+namespace TestShop.DAL.ADO
+{
+    public class ADOUnitOfWorkcs : IUnitOfWork
+    {
+        SqlConnection connection;
+
+        public ADOUnitOfWorkcs(string connectionString)
+        {
+            connection = new SqlConnection(connectionString);
+        }
+
+        public IProductRepository Products
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public IProductRepository Customers
+        {
+            get
+            {
+                return null;
+            }
+        }
+    }
+}
